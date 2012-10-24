e30b6442c131fdf5b55c21827a0f68e52f42409a
Peter Halliday <pghalliday@gmail.com>
2012-10-24 23:30:40 +0200

    initial imprt from tls-tunnel

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 single-tls-tunnel
-=================
+=========
+
+A single port TLS tunnel implementation to support a single downstream client proxy
+
+## Features
+
+- List features
+
+## Installation
+
+```
+npm install single-tls-tunnel
+```
+
+## API
+
+```javascript
+var SOMETHING = require('single-tls-tunnel');
+```
+
+## Roadmap
+
+- List backlog items
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using ``./grunt.sh`` or ``.\grunt.bat``.
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2012 Peter Halliday  
+Licensed under the MIT license.
---
+++ grunt.js
@@ -0,0 +1,94 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-shell');
+
+  function getLintConfig() {
+    return {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        es5: true,
+        strict: false
+      },
+      globals: {
+      }
+    };
+  }
+  
+  function getNodeLintConfig() {
+    var config = getLintConfig();
+    config.options.node = true;
+    return config;
+  }
+
+  function getNodeTestLintConfig() {
+    var config = getNodeLintConfig();
+    config.globals.describe = false;
+    config.globals.it = false;
+    config.globals.before = false;
+    config.globals.after = false;
+    config.globals.beforeEach = false;
+    config.globals.afterEach = false;
+    return config;
+  }
+
+  // Project configuration.
+  grunt.initConfig({
+    lint: {
+      node: ['grunt.js', 'src/**/*.js'],
+      nodeTest: ['test/src/**/*.js']
+    },
+    jshint: {
+      node: getNodeLintConfig(),
+      nodeTest: getNodeTestLintConfig()
+    },
+    mochaTest: {
+      test: ['test/src/**/*.js'],
+      doc: ['test/src/**/*.js']
+    },
+    mochaTestConfig: {
+      test: {
+        options: {
+          reporter: 'nyan'        
+        }
+      },
+      doc: {
+        options: {
+          reporter: 'doc'        
+        }
+      }
+    },
+    shell: {
+      generateKeys: {
+        command: (process.platform === 'win32') ? '.\\keys.bat' : './keys.sh',
+        execOptions: {
+          cwd: './test/keys'
+        }
+      },
+      _options: {
+        stdout: true,
+        stderr: true,
+        failOnError: true
+      },
+    },
+    watch: {
+      files: ['grunt.js', 'src/**/*.js', 'test/src/**/*.js'],
+      tasks: ['default']
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint shell:generateKeys mochaTest:test');
+
+  // Documentation task.
+  grunt.registerTask('doc', 'mochaTest:doc');
+};
