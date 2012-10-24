85a473c28d5223eb68450ede4b1dab91505bc086
Peter Halliday <pghalliday@gmail.com>
2012-10-25 00:08:47 +0200

    Client should make a TLS connection to the server

--- src/Client.js
+++ src/Client.js
@@ -1,5 +1,21 @@
-function Client() {
+var tls = require('tls');
+
+function Client(options) {
+  var self = this,
+      connection;
   
+  self.connect = function(callback) {
+    connection = tls.connect(options, function() {
+      callback();
+    });
+  };
+  
+  self.disconnect = function(callback) {
+    connection.on('end', function() {
+      callback();
+    });
+    connection.end();
+  };
 }
 
 module.exports = Client;
--- test/src/Client.js
+++ test/src/Client.js
@@ -1,9 +1,61 @@
 var expect = require('chai').expect,
+    tls = require('tls'),
+    net = require('net'),
+    MultiplexStream = require('multiplex-stream'),
+    Checklist = require('checklist'),
+    fs = require('fs'),
     Client = require('../../src/Client');
+
+var PORT = 8080,
+    SERVER_KEY = fs.readFileSync('./test/keys/server-key.pem'),
+    SERVER_CERT = fs.readFileSync('./test/keys/server-cert.pem'),
+    CLIENT_KEY = fs.readFileSync('./test/keys/client-key.pem'),
+    CLIENT_CERT = fs.readFileSync('./test/keys/client-cert.pem');
+
+var SERVER_OPTIONS = {
+  key: SERVER_KEY,
+  cert: SERVER_CERT,
+  ca: [CLIENT_CERT], 
+  requireCert: true,
+  rejectUnauthorized: true
+};
+
+var CLIENT_OPTIONS = {
+  port: PORT,
+  key: CLIENT_KEY,
+  cert: CLIENT_CERT,
+  ca: [SERVER_CERT],
+  rejectUnauthorized: true
+};
     
 describe('Client', function() {
-  it('should construct', function(done) {
-    var client = new Client();
-    done();
+  it('should make a TLS connection to the server', function(done) {
+    var client = new Client(CLIENT_OPTIONS);
+    var checklist = new Checklist([
+      'connection',
+      'end',
+      'undefined',
+      'undefined',
+      'undefined',
+      'undefined'
+    ], done);
+    var server = tls.createServer(SERVER_OPTIONS, function(connection) {
+      checklist.check('connection');
+      connection.on('end', function() {
+        checklist.check('end');
+      });
+    });
+    server.listen(PORT, function(error) {
+      checklist.check(typeof error);
+      client.connect(function(error) {
+        checklist.check(typeof error);
+        client.disconnect(function(error) {
+          checklist.check(typeof error);
+          server.close(function(error) {
+            checklist.check(typeof error);
+          });    
+        });
+      });
+    });
   });
 });
