edd6b83c3c94dfde7a04b48be1a38fa3285c2819
Peter Halliday <pghalliday@gmail.com>
2012-10-25 23:03:21 +0200

    http upgrade should work

--- test/spikes/httpUpgrade.js
+++ test/spikes/httpUpgrade.js
@@ -1,29 +1,64 @@
-var http = require('http')
-    tls = require('tls');
+var http = require('http'),
+    tls = require('tls'),
+    fs = require('fs'),
+    crypto = require('crypto'),
+    net = require('net');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('okay');
+var PORT = 8080,
+    SERVER_KEY = fs.readFileSync('./test/keys/server-key.pem'),
+    SERVER_CERT = fs.readFileSync('./test/keys/server-cert.pem'),
+    CLIENT_KEY = fs.readFileSync('./test/keys/client-key.pem'),
+    CLIENT_CERT = fs.readFileSync('./test/keys/client-cert.pem');
+
+var httpServer = http.createServer();
+httpServer.on('connection', function(connection) {
+  console.log('connection');
 });
-server.on('upgrade', function(req, socket, head) {
-  socket.write('HTTP/1.1 101 Web Socket Protocol Handshake\r\n' +
-               'Upgrade: WebSocket\r\n' +
+httpServer.once('upgrade', function(req, socket, head) {
+  socket.write('HTTP/1.1 200\r\n' +
+               'Upgrade: TLS\r\n' +
                'Connection: Upgrade\r\n' +
                '\r\n');
+               
+  var securePair = tls.createSecurePair(
+   crypto.createCredentials({
+     key: SERVER_KEY,
+     cert: SERVER_CERT,
+     ca: [CLIENT_CERT]
+   }),
+   true,
+   true,
+   true
+  );
+  var cleartext = securePair.cleartext,
+      encrypted = securePair.encrypted;
 
-  socket.pipe(socket); // echo back
+  socket.pipe(encrypted).pipe(socket);
+    
+  securePair.on('secure', function() {
+    console.log('server secure');
+  });
+    
+  cleartext.setEncoding();
+  cleartext.on('data', function(data) {
+    console.log(data);
+    cleartext.write('Hello, client');
+  });
+    
+  cleartext.on('end', function() {
+    console.log('end');
+  });
 });
 
 // now that server is running
-server.listen(1337, '127.0.0.1', function() {
+httpServer.listen(PORT, function() {
 
   // make a request
   var options = {
-    port: 1337,
-    host: '127.0.0.1',
+    port: PORT,
     headers: {
       'Connection': 'Upgrade',
-      'Upgrade': 'websocket'
+      'Upgrade': 'TLS'
     }
   };
 
@@ -31,8 +66,45 @@ server.listen(1337, '127.0.0.1', function() {
   req.end();
 
   req.on('upgrade', function(res, socket, upgradeHead) {
-    console.log('got upgraded!');
-    socket.end();
-    process.exit(0);
+    var securePair = tls.createSecurePair(
+     crypto.createCredentials({
+       key: CLIENT_KEY,
+       cert: CLIENT_CERT,
+       ca: [SERVER_CERT]
+     }),
+     false,
+     true,
+     true
+    );
+    var cleartext = securePair.cleartext,
+        encrypted = securePair.encrypted;
+
+    socket.pipe(encrypted).pipe(socket);
+
+    securePair.on('secure', function() {
+      console.log('client secure');
+      cleartext.write('Hello, server');
+    });
+
+    cleartext.setEncoding();
+    cleartext.on('data', function(data) {
+      console.log(data);
+      var connection = net.connect(PORT, function() {
+        connection.write('Hello');
+      });
+      connection.on('error', function(error) {
+        console.log(error);
+      });
+      http.get('http://localhost:' + PORT, function(res) {
+        console.log(res.statusCode);
+      });
+    });
+
+    cleartext.on('end', function() {
+      server.close(function() {
+        console.log('finished');
+      });
+    });
   });
 });
+
